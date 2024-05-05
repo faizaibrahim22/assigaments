6fc8381c1c9fea42c2e2a083157151836a42d8a0
faizaibrahim22 <faizaibrahim14789@gmail.com>
2024-05-05 04:28:54 -0700

    post

---
+++ for.js
@@ -0,0 +1,8 @@
+/*let  tab:number =5
+for(let i:number = 1;i<=10;i++){
+   console.log(`${tab} * ${i} = ${tab*i}`)
+}*/
+var table = 4;
+for (var t = 1; t <= 20; t++) {
+    console.log("".concat(table, " * ").concat(t, " ").concat(table * t));
+}
---
+++ for.ts
@@ -0,0 +1,10 @@
+#!/use/bin/env node
+let  tab:number =5 
+for(let i:number = 1;i<=10;i++){
+   console.log(`${tab} * ${i} = ${tab*i}`)
+}
+let table:number =4
+for(let t:number = 1;t<=20;t++){
+  console.log(`${table} * ${t} ${table*t}`)
+}
+
